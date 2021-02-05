970df4206c9ab82245d2fee69a7c0bf487b81b23
deeddeed <nemethdanielwebdesign@gmail.com>
2021-02-05 12:58:42 +0100

    Added Random Color Buttons Background

--- myscript.js
+++ myscript.js
@@ -1,37 +1,60 @@
-const grid = document.getElementById("gridTable");
-const resetBtn = document.getElementById("resetBtn");
+//BUTTONS
+let resetBtn = document.getElementById("resetBtn");
 let mouseMoveBtn = document.getElementById("mouseMoving");
 let mouseClickBtn = document.getElementById("mouseClick");
+let randomPaint = document.getElementById("randomPaint");
+let randomBackground = document.getElementById("randomBackground");
 const userPickedColor = document.getElementById("userColor");
 const userPickedMoveColor = document.getElementById("userMoveColor");
+
+const grid = document.getElementById("gridTable");
+
 let clickColor = "#FA824C";
-let gridSize = 2184;
 let moveColor = "#A2D729";
 let slider = 15;
+let gridSize = 2184;
+let divColor = "rgb(255,255,255)";
 
-//createGrid(gridSize);
-console.log(slider.value);
+console.log(divColor);
 createGrid(gridSize);
 
+//STAGE I
+//create the grid and distribute the squares
+function createGrid(gridSize) {
+  for (let j = 0; j < gridSize; j++) {
+    grid.appendChild(createDiv(slider));
+  }
+}
+//STAGE II
 // create the size of the squares
 function createDiv(size) {
   const div = document.createElement("div");
   div.classList.add("square");
+  div.style.backgroundColor = divColor;
   div.style.width = `${size}px`;
   div.style.height = `${size}px`;
-  console.log(size);
+
   return div;
 }
 
-//create the grid and distribute the squares
-function createGrid(gridSize) {
-    for (let j = 0; j < gridSize; j++) {
-      grid.appendChild(createDiv(slider));
-      console.log(gridSize + " j ist " + j);
-    }
+//change the Background to a random color (divs)
+function changeBackground(){
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  divColor =   "rgb( " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " )";  
+  createGrid(gridSize);
+  randomBackground.style.backgroundColor = divColor;
 }
+
 //change the color the user picks for the MOUSE MOVE FUNCTION
-function userMoveColorPick(event){
+function userMoveColorPick(event) {
   moveColor = event.target.value;
   mouseMoveBtn.style.backgroundColor = moveColor;
 }
@@ -40,11 +63,32 @@ function userColorPicked(event) {
   clickColor = event.target.value;
   mouseClickBtn.style.backgroundColor = clickColor;
 }
-
+//random color mouse CLICK BUTTON
+function mouseClickBtnRandom(){
+    clickColor = "rgb( " +
+    Math.floor(Math.random() * (0 + 255)) +
+    " " +
+    Math.floor(Math.random() * (0 + 255)) +
+    " " +
+    Math.floor(Math.random() * (0 + 255)) +
+    " )";  
+    mouseClickBtn.style.backgroundColor= clickColor;
+}
+//random color mouse MOVE BUTTON
+function mouseMoveBtnRandom(){
+  moveColor = "rgb( " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " " +
+  Math.floor(Math.random() * (0 + 255)) +
+  " )";  
+  mouseMoveBtn.style.backgroundColor= moveColor;
+}
 // Change the color with mouse click
 grid.addEventListener("click", function (e) {
   if (e.target.matches(".square")) {
-    e.target.style.backgroundColor = clickColor; 
+    e.target.style.backgroundColor = clickColor;
     console.log("mouse is CLICKING");
     console.log(clickColor);
   }
@@ -63,11 +107,18 @@ function reset() {
   while (grid.firstChild) {
     grid.removeChild(grid.lastChild);
   }
+  divColor = "rgb(255,255,255)";
   createGrid(gridSize);
 }
 
 resetBtn.addEventListener("click", reset);
+randomBackground.addEventListener("click", changeBackground);
 userPickedColor.addEventListener("change", userColorPicked, false);
 userPickedMoveColor.addEventListener("change", userMoveColorPick, false);
+mouseClickBtn.addEventListener("click", mouseClickBtnRandom);
+mouseMoveBtn.addEventListener("click", mouseMoveBtnRandom);
+
+
+randomBackground.style.backgroundColor = divColor;
 mouseClickBtn.style.backgroundColor = clickColor;
 mouseMoveBtn.style.backgroundColor = moveColor;
